6d3ccf424c66d6797099213faf957b1fad010bb4
gencms <generatecms@gmail.com>
2014-04-16 11:23:01 +0300

    Removed the obsolete functions and used CI built in functions

--- app/modules/news/controllers/news.php
+++ app/modules/news/controllers/news.php
@@ -82,14 +82,14 @@
 		 */
 
 		function items() {										
-		        $join_items[] = array(
-		                 "table"        =>  "news_category"
-		                ,"key_field"    =>  "news_category_id"
-		                ,"mapped_field" =>  "news_category"
-		                ,"join_type"    =>  "inner"
-		        );
-				
-				$aColumns = array(news_id,title,picture,category_name,news_date,active,1);	
+	        $join_items[] = array(
+	                 "table"        =>  "news_category"
+	                ,"key_field"    =>  "news_category_id"
+	                ,"mapped_field" =>  "news_category"
+	                ,"join_type"    =>  "inner"
+	        );
+			
+			$aColumns = array(news_id,title,picture,category_name,news_date,active,1);	
     
 		    /* Indexed column (used for fast and accurate table cardinality) */
 		    $sIndexColumn = "news_id";
